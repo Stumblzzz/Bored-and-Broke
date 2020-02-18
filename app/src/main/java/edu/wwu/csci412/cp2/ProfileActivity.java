f755ede09b9d4fe69645a89bc63070e05439cc46
Matthew Carter <40812910+matcarter@users.noreply.github.com>
2020-02-17 22:56:51 -0800

    Add Profile layout and activity skeleton

---
+++ app/src/main/java/edu/wwu/csci412/cp2/ProfileActivity.java
@@ -0,0 +1,18 @@
+package edu.wwu.csci412.cp2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void updateView() {
+
+    }
+}
